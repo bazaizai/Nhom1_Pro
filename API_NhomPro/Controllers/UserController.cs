236296f57717680fa36ef402ee1943137606db28
DESKTOP-8RSAFN0\anhtr <anhtrinhtran3@gmail.com>
2023-05-23 21:50:16 +0700

    controller role

---
+++ API_NhomPro/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using AppData.IRepositories;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nhom1_Pro.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IAllRepo<User> repos;
+        DBContextModel dbContextModel = new DBContextModel();
+        DbSet<User> Users;
+        public UserController()
+        {
+            Users = dbContextModel.Users;
+            AllRepo<User> all = new AllRepo<User>(dbContextModel, Users);
+            repos = all;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return repos.GetAll();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
