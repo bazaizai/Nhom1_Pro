93ce6963db409539799c284ae56a55e3de447680
BAZAIZAI\ASUS <hieuhxptto@gmail.com>
2023-06-04 02:40:18 +0700

    cha lam duoc cai gi

---
+++ AppView/Controllers/QuanLyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppView.Controllers
+{
+    public class QuanLyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
